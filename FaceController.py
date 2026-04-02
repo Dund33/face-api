3914a85e0e4b6c086cd42fd11add49f56af980eb
Dund33 <warior1112@gmail.com>
2026-04-02 22:46:31 +0200

    Removed leftover code and reformatted the file

--- FaceController.py
+++ FaceController.py
@@ -15,7 +15,9 @@ from VectorStore import RedisVectorStore
 load_dotenv()
 
 min_face_confidence = os.getenv("MIN_FACE_CONFIDENCE")
-min_face_confidence = float(min_face_confidence) if min_face_confidence is not None else 0
+min_face_confidence = (
+    float(min_face_confidence) if min_face_confidence is not None else 0
+)
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 API_USER = os.getenv("API_USER")
@@ -25,34 +27,29 @@ POS_ID_THRESH = float(POS_ID_THRESH) if POS_ID_THRESH is not None else 0
 vector_store = RedisVectorStore(dim=4096)
 vector_store.create_index()
 app = FastAPI()
+security = HTTPBearer()
+
 
 def get_face_embedding(image_path):
     """Extract face embedding from an image"""
     try:
         embedding_objs = DeepFace.represent(image_path)
-        face_confidence = embedding_objs[0]['face_confidence']
+        face_confidence = embedding_objs[0]["face_confidence"]
 
         if face_confidence < min_face_confidence:
             raise FaceNotDetected
 
-        embedding = embedding_objs[0]['embedding']
+        embedding = embedding_objs[0]["embedding"]
         embedding = np.array(embedding)
         return embedding
-    
+
     except FaceNotDetected:
-        print('ERROR: FACE NOT DETECTED')
+        print("ERROR: FACE NOT DETECTED")
         return None
-
-images: Dict[str, dict] = {}
-
-security = HTTPBearer()
 
 
 def create_token(subject: str):
-    payload = {
-        "sub": subject,
-        "exp": datetime.utcnow() + timedelta(hours=24)
-    }
+    payload = {"sub": subject, "exp": datetime.utcnow() + timedelta(hours=24)}
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 
@@ -78,7 +75,7 @@ def get_token(username: str, password: str):
 async def register_image(
     id: str = Form(...),
     image: UploadFile = File(...),
-    subject: str = Depends(verify_token)
+    subject: str = Depends(verify_token),
 ):
     image_id = str(uuid.uuid4())
     os.makedirs("uploads", exist_ok=True)
@@ -94,9 +91,7 @@ async def register_image(
 
 
 @app.get("/identify")
-async def identify(id: str = Form(...),
-             image: UploadFile = File(...),
-             subject: str = Depends(verify_token)):
+async def identify(image: UploadFile = File(...), subject: str = Depends(verify_token)):
     image_id = str(uuid.uuid4())
     os.makedirs("uploads", exist_ok=True)
     file_path = f"uploads/{image_id}_{image.filename}"
@@ -106,6 +101,6 @@ async def identify(id: str = Form(...),
 
     if embedding is not None:
         found = vector_store.search(embedding)
-        found_filtered = list(filter(lambda x: x['score'] <= POS_ID_THRESH, found))
+        found_filtered = list(filter(lambda x: x["score"] <= POS_ID_THRESH, found))
         return {"found": found_filtered}
-    return {}
+    return {"found": []}
